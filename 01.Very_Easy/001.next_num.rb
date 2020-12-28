124cb856f5c0f1958f1ac91beee0eefe50a44adf
JuzerShakir <juz.shakir.software@gmail.com>
2020-12-28 10:33:41 +0530

    add spacing

--- 01.Very_Easy/001.next_num.rb
+++ 01.Very_Easy/001.next_num.rb
@@ -13,7 +13,9 @@ end
 
 # asks user to enter a number
 print "Enter a number you want to increament by 1: "
+
 # num var takes the user input, '.chop' clears the 'enter' key user presses while submitting the value and '.to_i; converts input to int
 num = gets.chop.to_i
+
 # calls function and gives output
 puts next_num num
