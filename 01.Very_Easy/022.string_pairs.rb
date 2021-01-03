7d94fe1c206f9a1cb1dabaf6c80928e9b7abfc91
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-03 09:56:24 +0530

    explain

--- 01.Very_Easy/022.string_pairs.rb
+++ 01.Very_Easy/022.string_pairs.rb
@@ -15,17 +15,25 @@ See the below examples for a better understanding:
 Return [] if the given string is empty.
 =end
 
+
 def pairs(str)
+  # a string method which extracts 2 letters, hence 2 dots
+  # ...return an array where each element is 2 letters of str
   str.scan(/../)
 end
 
+# ask user for input
 print "Enter any word: "
 str = gets.chomp
 
+# calculates length of str and then checks if its remainder by 2
 str_len = str.length % 2
 #p str_len
 
+# if str length is even then remainder would be 0
+# ...if its odd then 1
 if str_len != 0
+  # if str_len is 1 then we add * to make the length of str even
   str << '*'
 end
 #p str
