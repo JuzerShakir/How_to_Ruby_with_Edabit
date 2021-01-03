28c02240b30717850ee83076b20c6e3a178d55b0
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-03 15:38:50 +0530

    ask user for input

--- 01.Very_Easy/023.Two_makes_Ten.rb
+++ 01.Very_Easy/023.Two_makes_Ten.rb
@@ -12,3 +12,10 @@ makes10(9, 9) ➞ false
 makes10(1, 9) ➞ true
 
 =end
+
+puts "Enter any 2 numbers, each separated by comma:"
+num1, num2 = gets.chomp.split(',')
+
+p num1.to_i
+p num2.to_i
+#p makes10(num1, num2)
