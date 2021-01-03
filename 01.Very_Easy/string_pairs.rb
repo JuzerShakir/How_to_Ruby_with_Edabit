5901149a00e28bd06ca53d8ded82521db658fbdf
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-03 09:23:11 +0530

    Add Description

---
+++ 01.Very_Easy/string_pairs.rb
@@ -0,0 +1,16 @@
+############ Created on 03-01-21
+=begin
+Create a function which takes an argument of string str and
+returns an array of two-paired characters. If the string has
+odd number of characters, add an asterisk * in final pair.
+
+See the below examples for a better understanding:
+
+  string_pairs("mubashir") ➞ ["mu", "ba", "sh", "ir"]
+
+  string_pairs("edabit") ➞ ["ed", "ab", "it"]
+
+  string_pairs("airforces") ➞ ["ai", "rf", "or", "ce", "s*"]
+
+Return [] if the given string is empty.
+=end
