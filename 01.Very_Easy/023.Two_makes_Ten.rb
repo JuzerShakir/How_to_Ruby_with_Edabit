9b0d53b994ef1cbae8faf6c6a0bf9d483f155100
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-03 16:10:15 +0530

    Explain

--- 01.Very_Easy/023.Two_makes_Ten.rb
+++ 01.Very_Easy/023.Two_makes_Ten.rb
@@ -14,18 +14,20 @@ makes10(1, 9) ➞ true
 =end
 
 def makes10(num1, num2)
-
+  # totals user input to see if it equals to 10
   total = num1 + num2
 
+  # checks if any of these vars are 10, if yes then true
   if num1 == 10 || num2 == 10 || total == 10 then true else false end
-
 
 end
 
+#asks for user input
 puts "Enter any 2 numbers, each separated by comma:"
 num1, num2 = gets.chomp.split(',')
 
 #p num1.to_i
 #p num2.to_i
 
+#calls function by converting argys str to int
 p makes10(num1.to_i, num2.to_i)
