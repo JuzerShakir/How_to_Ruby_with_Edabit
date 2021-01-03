d26ac558c1aa4a03c981a851977654b8a51da17e
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-03 16:08:00 +0530

    Solution

--- 01.Very_Easy/023.Two_makes_Ten.rb
+++ 01.Very_Easy/023.Two_makes_Ten.rb
@@ -13,9 +13,19 @@ makes10(1, 9) ➞ true
 
 =end
 
+def makes10(num1, num2)
+
+  total = num1 + num2
+
+  if num1 == 10 || num2 == 10 || total == 10 then true else false end
+
+
+end
+
 puts "Enter any 2 numbers, each separated by comma:"
 num1, num2 = gets.chomp.split(',')
 
-p num1.to_i
-p num2.to_i
-#p makes10(num1, num2)
+#p num1.to_i
+#p num2.to_i
+
+p makes10(num1.to_i, num2.to_i)
