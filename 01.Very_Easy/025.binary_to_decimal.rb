0d7d41b3f623c8e9a8b867b2d719226a85e472d1
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-12 09:19:09 +0530

    Define problem

---
+++ 01.Very_Easy/025.binary_to_decimal.rb
@@ -0,0 +1,13 @@
+######### Build on 12 Jan 2021
+=begin
+In mathematics and digital electronics, a binary number
+is a number expressed in the base-2 numeral system or
+binary numeral system. Given an array of ones and zeroes
+of a binary number, return the equivalent decimal value.
+
+  binary_to_decimal([0, 0, 0, 1]) ➞ 1
+
+  binary_to_decimal([0, 0, 1, 0]) ➞ 2
+
+  binary_to_decimal([1, 1, 1, 1, 1, 0, 1, 1, 0, 1]) ➞ 1005
+=end
