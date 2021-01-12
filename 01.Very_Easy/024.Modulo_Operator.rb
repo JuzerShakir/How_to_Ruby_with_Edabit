bd6142870bc691abc229e3157f82d1076d8739b1
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-12 09:00:19 +0530

    Define Problem

---
+++ 01.Very_Easy/024.Modulo_Operator.rb
@@ -0,0 +1,14 @@
+############ Build on 12 Jan 2021
+=begin
+Create a function which returns the Modulo of the two
+given numbers.
+
+    mod(-13, 64) ➞ 51
+
+    mod(50, 25) ➞ 0
+
+    mod(-6, 3) ➞ 0
+
+All the test cases should contain valid numbers.
+
+=end
