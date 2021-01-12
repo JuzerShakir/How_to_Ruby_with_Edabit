770b32c25e1c39da30d90232b9ed7247e964da56
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-12 09:09:18 +0530

    Explain

--- 01.Very_Easy/024.Modulo_Operator.rb
+++ 01.Very_Easy/024.Modulo_Operator.rb
@@ -14,10 +14,13 @@ All the test cases should contain valid numbers.
 =end
 
 def mod(n1, n2)
+  #returns modulus or remainder of the division
   return n1 % n2
 end
 
+# asks for user input
 puts "Enter 2 valid numbers each separated by comma: "
 n1, n2 = gets.chomp.split(',')
 
+# calling func by giving arguments converting to an int form
 p mod(n1.to_i, n2.to_i)
