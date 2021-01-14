33c72610444756a03413c6f6d43d4b509d3b03e2
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-14 08:43:18 +0530

    Define problem

---
+++ 01.Very_Easy/026.Sum_of_Polygon_Angles.rb
@@ -0,0 +1,17 @@
+############# Created at 14 Jan 2021
+=begin
+Given an n-sided regular polygon n, return the total
+sum of internal angles (in degrees).
+
+  sum_polygon(3) ➞ 180
+
+  sum_polygon(4) ➞ 360
+
+  sum_polygon(6) ➞ 720
+
+Notes:
+  n will always be greater than 2.
+  The formula (n - 2) x 180 gives the sum of all the
+  measures of the angles of an n-sided polygon.
+=end
+
