eff2ab500c7e530c49576b86b6b877808ac2afc6
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-14 08:50:50 +0530

    Explain

--- 01.Very_Easy/026.Sum_of_Polygon_Angles.rb
+++ 01.Very_Easy/026.Sum_of_Polygon_Angles.rb
@@ -16,11 +16,14 @@ Notes:
 =end
 
 def sum_polygon(n)
+  # formula already given in problem
   ans = (n - 2) * 180
   return ans
 end
 
+# asks user for input
 print 'Enter no. of sides of polygons: '
 n = gets.chomp.to_i
 
+# calls function
 p sum_polygon(n)
