78f2edbe6097fc89ec5d7ea5b09453c877591416
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-14 09:56:57 +0530

    Explain

--- 01.Very_Easy/027.Largest_nums.rb
+++ 01.Very_Easy/027.Largest_nums.rb
@@ -16,20 +16,30 @@ The returned array must be sorted in ascending order.
 =end
 
 def largest_numbers(n, arr)
+  # empty array for holding largest numbers
   arr_max = []
 
+  #will loop through array to find largest nums as many..
+  #..as defined by user
   while n > 0
+    # appends highest num in array
     arr_max << arr.max
+    # deletes high num from user defined array
     arr.delete(arr.max)
+    # decreamenting count
     n-=1
   end
 
+  # since values of arrays are sorted in descending order..
+  # .. we give sort method to sort in ascending order
   return arr_max.sort!
 end
 
+# asks user for an array
 puts 'Enter an array of numbers, each separated by comma: '
 arr = gets.chomp.split(',').map(&:to_i)
 
+# asks user for number of largest numbers
 print 'How many largest no.s you want? '
 n = gets.chomp.to_i
 
