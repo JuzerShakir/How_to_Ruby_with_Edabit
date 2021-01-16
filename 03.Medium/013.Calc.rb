69a7e0b87789000b3593bdde84f82051729a2759
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-16 07:23:36 +0530

    remove unrequired var

--- 03.Medium/013.Calc.rb
+++ 03.Medium/013.Calc.rb
@@ -1,6 +1,6 @@
 ########## Build on 03.12.2020
 =begin
-Create a function that takes two numbers and a mathematical operator + - / * 
+Create a function that takes two numbers and a mathematical operator + - / *
 and will perform a calculation with the given numbers.
 
 calculator(2, "+", 2) ➞ 4
@@ -19,18 +19,18 @@ def calc a, operator, b
 	case operator
 		# add
 		when '+'
-			total = a + b
+			a + b
 		# subs
 		when '-'
-			total = a - b
+			a - b
 		# multiply
 		when '*'
-			total = a * b
+			a * b
 		# division
 		when '/'
 			# since division by 0 raises an error...
 			begin
-				total = a / b
+        a / b
 			rescue ZeroDivisionError
 				# prints this message out instead of raising an error
 				"Can't divide by 0!"
@@ -41,7 +41,7 @@ end
 puts "Enter any 2 numbers that you want to perform calculations with, separated by comma: "
 a, b = gets.chomp.split(',').map(&:to_i)
 
-puts "Choose which calculation to perform on those nums '+,-,/ or *': " 
+puts "Choose which calculation to perform on those nums '+,-,/ or *': "
 operator = gets.chomp
 
 puts calc a, operator, b
