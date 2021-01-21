06cbdac262b5221258898c3d2c8939232d6d78ff
JuzerShakir <juz.shakir.software@gmail.com>
2021-01-21 10:29:21 +0530

    Explain

--- 04.Hard/001.Sum_of_missing_num.rb
+++ 04.Hard/001.Sum_of_missing_num.rb
@@ -19,18 +19,33 @@ between the minimum and maximum of the numbers (inclusive).
 =end
 
 def sum_missing_nums(list)
+  # extract min and max value from user input and make a list with that range
   seq_list = (list.min..list.max).to_a
 
+  # now we want to know whether the input array really had any missing numbers
+  # the only way to know that is by seeing if both arrays are same
+  # since == operator looks for corresponding element values and
+  # seq_list is sorted in ascd order we give input list sort method
+  if seq_list == list.sort
+    # the input had no missing value, hence end program
+    return "No Missing Numbers (i.e. all numbers in [#{list.min}, #{list.max}] are present in the list)"
+  end
+
+  # now we substract the lists, and we get the missing nums of that range that user didnt enter
   missing_list = seq_list - list
 
+  # initializing total
   total = 0
+  # loops through and adds each number
   missing_list.each do | num | total += num end
 
-  if total == 0 then "No Missing Numbers (i.e. all numbers in [#{list.min}, #{list.max}] are present in the list"
-                  else return total end
+  return total
 end
 
+#asks user for input
 puts "Enter list of numbers each separated by comma:"
+# converts input str to int
 list = gets.chomp.split(',').map(&:to_i)
 
+# calls the func
 puts sum_missing_nums(list)
