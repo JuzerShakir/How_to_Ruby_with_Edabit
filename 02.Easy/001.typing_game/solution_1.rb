2617a072787c6f9c5bba69c86b3b55406c7d8626
JuzerShakir <juz.shakir.software@gmail.com>
2021-03-07 20:00:51 +0530

    modified program and comment

--- 02.Easy/001.typing_game/solution_1.rb
+++ 02.Easy/001.typing_game/solution_1.rb
@@ -4,16 +4,26 @@
 def game user, correct
 	a = []
 
-	user.each_with_index do |element, idx| if element == correct[idx] then a << 1 else a << -1 end
-
-	end
-
+  # loops through first input with id
+	user.each_with_index do |element, idx|
+    # checks value of corresponding element of list is equal
+    if element == correct[idx]
+      # appends 1 if it is
+      a << 1
+    else
+      # appends -1 if its unequal
+      a << -1
+    end
+  end
+  #output
 	return a
 end
 
+# 1st input
 puts "Type any words, make sure each words are separated by comma: "
 user = gets.chomp.split(',')
 
+#2nd input
 puts "Type correct words here, make sure each words are separated by comma: "
 correct = gets.chomp.split(',')
 
