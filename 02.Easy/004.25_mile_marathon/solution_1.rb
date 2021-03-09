9b78aa878f72a0db0b45391c945df7bee88f073d
JuzerShakir <juz.shakir.software@gmail.com>
2021-03-09 17:25:28 +0530

    Add comment

--- 02.Easy/004.25_mile_marathon/solution_1.rb
+++ 02.Easy/004.25_mile_marathon/solution_1.rb
@@ -2,10 +2,12 @@
 ########## Ruby version ruby 2.7.2p137
 
 def distance len
+  # updates list by taking absolute value of each element
 	len.map! {|n| n.abs}
-
+  # gives total of all elements
 	total_len = len.sum()
 
+  # check condition
 	if total_len == 25 then true else false end
 end
 
