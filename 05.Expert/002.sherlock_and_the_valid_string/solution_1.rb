f95663327b5240606aa8aef926473f2948ebd659
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-01-06 18:58:08 +0530

    Updated logic to consider the length of the other characters when there's 1 occurance of any char

--- 05.Expert/002.sherlock_and_the_valid_string/solution_1.rb
+++ 05.Expert/002.sherlock_and_the_valid_string/solution_1.rb
@@ -11,16 +11,16 @@ def is_valid (str)
 
     # returning hash, grouping by count number, which tells us how many characters have similar counts
     grp_char_count = char_count.values.group_by { | v | v }
-    #p grp_char_count
+    p grp_char_count
 
     grp_char_occurance = Array.new
     # The value of key is list, the length of each list tells how many chars are of that same count
     grp_char_count.each { | k , v | grp_char_occurance.append(v.count) }
-    #p grp_char_occurance
+    p grp_char_occurance
 
     # if user has input just a single char, or..
-    # if user has input a char whose occurance in string is just once
-    if grp_char_occurance.length == 1 || grp_char_occurance.any?(1)
+    # if user has input a char whose occurance in string is just once and the occurance of other char in input is same
+    if grp_char_occurance.length == 1 || (grp_char_occurance.length <=2 and grp_char_occurance.any?(1))
         return "YES"
     else
         return "NO"
