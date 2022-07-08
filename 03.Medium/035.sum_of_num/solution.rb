47b18aabeedfe886a1f00325ad112aa206730b2c
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-07-08 09:19:23 +0530

    new exercises

--- 03.Medium/035.sum_of_num/solution.rb
+++ 03.Medium/035.sum_of_num/solution.rb
@@ -1,3 +1,7 @@
+# def numbers_sum(arr)
+#     arr.select{ |n| n.is_a?(Numeric)}.reduce(0) { |acc, e| acc += e }
+# end
+
 def numbers_sum(arr)
-    arr.select{ |n| n.is_a?(Numeric)}.reduce(0) { |acc, e| acc += e }
+    arr.grep(Integer).reduce(0, :+)
 end
