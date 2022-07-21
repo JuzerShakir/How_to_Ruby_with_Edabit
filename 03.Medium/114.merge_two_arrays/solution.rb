56153d23d6712980e2d474dfeefd20e9a24e0f82
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-07-21 18:05:37 +0530

    more ways to solution

--- 03.Medium/114.merge_two_arrays/solution.rb
+++ 03.Medium/114.merge_two_arrays/solution.rb
@@ -1,4 +1,6 @@
 def merge_arrays(a1, a2)
-    a1_u = a1.push(*Array.new((a1.length-a2.length).abs))
+    # a1_u = a1.push(*Array.new((a1.length-a2.length).abs))
+    # a1_u = a1 + Array.new((a1.length-a2.length).abs)
+    a1_u = a1.concat(Array.new((a1.length-a2.length).abs))
     a1_u.zip(a2).flatten.compact
 end
